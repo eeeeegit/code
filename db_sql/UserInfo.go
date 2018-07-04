fb88b330c61208272757452732397778cf7b479b
erwin <rcbcerwin@gmail.com>
2018-07-04 15:17:40 +0800

    update

--- db_sql/UserInfo.go
+++ db_sql/UserInfo.go
@@ -5,22 +5,6 @@ import (
 )
 
 type IUserInfo interface {
-	//根据参数，自动完成数据库查询
-	Retrieve(params map[string]interface{}, args ...interface{}) map[string]interface{}
-	//根据参数，自动完成数据库插入
-	Create(params map[string]interface{}, args ...interface{}) map[string]interface{}
-	//根据参数，自动完成数据库更新（只支持单条）
-	Update(params map[string]interface{}, args ...interface{}) map[string]interface{}
-	//根据参数，自动完成数据库删除（只支持单条）
-	Delete(params map[string]interface{}, args ...interface{}) map[string]interface{}
-	//手写查询sql支持
-	QuerySql(sql string, values []interface{}, params map[string]interface{}) map[string]interface{}
-	//手写非查询sql支持
-	ExecSql(sql string, values []interface{}) map[string]interface{}
-	//批量插入或更新
-	InsertBatch(tablename string, els []interface{}) map[string]interface{}
-	//事务支持
-	TransGo(objs map[string]interface{}) map[string]interface{}
 }
 
 type UserInfo struct {
@@ -30,4 +14,8 @@ type UserInfo struct {
 	NickName   string `gorm:"default:'blue'"`
 	CreateTime time.Time
 	LoginTime  time.Time `gorm:"default:NOW()"`
+}
+
+func (user *UserInfo) Test() {
+
 }
